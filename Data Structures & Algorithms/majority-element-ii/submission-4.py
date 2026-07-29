9a8be14c6590188d4061fe8d833130d5c5a85c23
jagodawojcik <100490323+jagodawojcik@users.noreply.github.com>
2026-07-29 21:59:26 +0100

    Add: majority-element-ii - submission-4

---
+++ Data Structures & Algorithms/majority-element-ii/submission-4.py
@@ -0,0 +1,27 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        
+        count = defaultdict(int) # key: n, val: freq
+
+        for n in nums:
+            count[n] += 1
+
+            if len(count) <= 2:
+                continue
+
+            new_count = defaultdict(int)
+            for n, c in count.items():
+                if c > 1:
+                    new_count[n] = c - 1
+            count = new_count
+
+        
+        res = []
+
+        for n, c in count.items():
+            if nums.count(n) > len(nums) // 3:
+                res.append(n)
+
+        return res
+
+
