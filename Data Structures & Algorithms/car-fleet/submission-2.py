d8360cb6a83c44308fe1e22661cdbd510c4c87e2
jagodawojcik <100490323+jagodawojcik@users.noreply.github.com>
2026-08-13 22:06:43 +0100

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        
+        # time to reach target destination = (target - position) / speed
+        
+        # pos_speed = [[p, s]] for p, s in zip(position, speed)
+        pos_speed = []
+        for i in range(len(position)):
+            pos_speed.append((position[i], speed[i]))
+        
+        pos_speed.sort()
+        fleets = []
+        for p, s in pos_speed[::-1]:
+            fleets.append((target - p) / s)
+            if len(fleets) >= 2 and fleets[-1] <= fleets[-2]:
+                fleets.pop()
+
+        return len(fleets)
