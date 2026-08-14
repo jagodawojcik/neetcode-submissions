ae51d6bc5ee23d4ff1f30716bf5ec39f5011f717
jagodawojcik <100490323+jagodawojcik@users.noreply.github.com>
2026-08-14 21:38:35 +0100

    Add: simplify-path - submission-8

---
+++ Data Structures & Algorithms/simplify-path/submission-8.py
@@ -0,0 +1,28 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        
+        if path == "/":
+            return "/"
+
+        res = []
+        word = ""
+        # neetcode/practice/
+        # path="/neetcode/practice//...///../courses"
+        for c in path + '/':
+            if c == '/':
+                if word == "..":
+                    if res:
+                        res.pop()
+                elif word == ".":
+                    pass
+                else:
+                    if word:
+                        res.append(word)
+                word = ""
+            else:
+                word += c
+        
+        return "/" + "/".join(res)
+
+
+        
