b5c6b2e33e9e05f4fca69661e4000249651e3372
jagodawojcik <100490323+jagodawojcik@users.noreply.github.com>
2026-09-16 18:46:47 +0100

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # len(piles) <= h - always, because 1 pile per h max
+        # min k = 1, max k = max in piles
+        
+        l, r = 1, max(piles)
+        min_rate = r
+        while l <= r:
+            rate = (l + r) // 2
+            h_spent = 0
+            for p in piles:
+                h_spent += math.ceil(p / rate)
+
+            if h_spent > h:
+                l = rate + 1
+            else:
+                r = rate - 1
+                min_rate = min(rate, min_rate)
+
+        return min_rate
+
+
+
